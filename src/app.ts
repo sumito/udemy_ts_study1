073a65d5032c0c306b8003f048a9d109e4fff462
sumito <sumito.temma@gmail.com>
2023-03-03 02:04:58 +0900

    type casting

--- src/app.ts
+++ src/app.ts
@@ -45,6 +45,100 @@ function addCombine( a: Combinable,b:Combinable ){
   return a + b;
 }
 
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation( emp: UnknownEmployee ){
+  //console.log(emp);
+
+  if( 'privileges' in emp ){
+    console.log("Privileges: " + emp.privileges);
+  }
+  if( 'startDate' in emp ){
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+printEmployeeInformation({name: 'Max',startDate: new Date()});
+
+class Car {
+  drive(){
+    console.log("運転中...");
+  }
+}
+
+class Truck {
+  drive(){
+    console.log("トラックを運転中...");
+  }
+  loadCargo( amount: number ){
+    console.log("荷物を載せています..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle( vehicle : Vehicle ){
+  vehicle.drive();
+ //if( 'loadCargo' in vehicle ){
+  if( vehicle instanceof Truck ){
+      vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
+
+//discriminated unions: 判別可能なUnion型
+
+interface Bird{
+  type: 'bird',
+  flyingSpeed:number;
+}
+interface Horse{
+  type: 'horse',
+  runningSpeed:number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal( animal: Animal ){
+  
+  let speed;
+  switch( animal.type ){
+     case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+     case 'horse':
+      speed = animal.runningSpeed
+  }
+  console.log("移動速度："+speed);
+}
+
+moveAnimal({type: 'bird',flyingSpeed:1000});
+moveAnimal({type: 'horse',runningSpeed:20});
+
+
+//type casting
+
+//const paragraph = document.querySelector('p');
+//const userInputElement = <HTMLInputElement>document.getElementById('user-input');
+//const userInputElement = <HTMLInputElement>document.getElementById('user-input');
+const userInputElement = document.getElementById('user-input');
+if( userInputElement ){
+  //userInputElement.value = 'こんにちは';
+  ((userInputElement) as HTMLInputElement).value = 'こんにちは';
+}
+
+//index type
+
+
+
+//funciton overload
 
 
 
@@ -52,4 +146,3 @@ function addCombine( a: Combinable,b:Combinable ){
 
 
 
-//discriminated unions
